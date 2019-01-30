e1dbbc3336dc8576ea354f6ae06eb99be39df646
Anna <piladzisi@gmail.com>
2019-01-30 17:23:00 +0200

    removechild/parentNode

--- vid-1-using-parent-node/start/app.js
+++ vid-1-using-parent-node/start/app.js
@@ -5,18 +5,17 @@ const descriptionP = document.querySelector('p.description');
 const descriptionButton = document.querySelector('button.description');
 const addItemInput = document.querySelector('input.addItemInput');
 const addItemButton = document.querySelector('button.addItemButton');
-const removeItemButton = document.querySelector('button.removeItemButton');
+const listUL = listDiv.querySelector('ul');
 
-listDiv.addEventListener('mouseover', (event) => {
-  if (event.target.tagName == 'LI') {
-    event.target.textContent = event.target.textContent.toUpperCase();
+
+listUL.addEventListener('click', (event) => {
+  if (event.target.tagName == 'BUTTON') {
+   let li = event.target.parentNode;
+   let ul = li.parentNode;
+   ul.removeChild(li);
   }
 });
-listDiv.addEventListener('mouseout', (event) => {
-  if (event.target.tagName == 'LI') {
-    event.target.textContent = event.target.textContent.toLowerCase();
-  }
-});
+
 
 toggleList.addEventListener('click', () => {
   if (listDiv.style.display == 'none') {
@@ -41,11 +40,7 @@ addItemButton.addEventListener('click', () => {
   addItemInput.value = '';
 });
   
-removeItemButton.addEventListener('click', () => {
-  let ul = document.getElementsByTagName('ul')[0];
-  let li = document.querySelector('li:last-child');
-  ul.removeChild(li);
-});
+
   
   
   
